9c61af86afbb3388f47f8ff5393a7dbcc7eb47aa
Lane Faison <lane.faison@icloud.com>
2021-01-26 21:39:52 -0700

    App Icons and launch screen

--- bikeFinder/bikeFinder/Design/AppImages.swift
+++ bikeFinder/bikeFinder/Design/AppImages.swift
@@ -9,6 +9,7 @@ import UIKit
 
 final class AppImages {
     static let bicycle = UIImage(imageLiteralResourceName: "bicycle")
+    static let bicycle2 = UIImage(imageLiteralResourceName: "bicycle2")
     static let business = UIImage(imageLiteralResourceName: "business")
     static let locationMarker = UIImage(imageLiteralResourceName: "marker")
     static let zoomIn = UIImage(imageLiteralResourceName: "zoomIn")
